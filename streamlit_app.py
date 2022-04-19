7648858a1e660c1a447188be1af1f7f2624d8f95
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-19 14:20:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,19 @@
 import streamlit as st
 
+st.title('st.form')
+
+st.header('Declared using the `with` statement')
+with st.form('my_form'):
+    st.write('Inside the form')
+    slider_val = st.slider('Form slider')
+    checkbox_val = st.checkbox('Form checkbox')
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        st.write('slider', slider_val, 'checkbox', checkbox_val)
+
+st.write('Outside the form')
+
+#st.header('Declared using object notation')
+#form = st.form('my_form_2')
