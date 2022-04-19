8fae803993c055012faa423f6d4a316edc9176dd
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-19 14:53:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,23 @@ st.title('st.form')
 
 st.header('Declared using the `with` statement')
 with st.form('my_form'):
-    st.write('Inside the form')
+    st.write('**Order your coffee**')
+    
+    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
     cofee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-    checkbox_val = st.checkbox('Form checkbox')
+    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+    milk_val = st.checkbox('Milk')
 
     # Every form must have a submit button.
     submitted = st.form_submit_button('Submit')
     if submitted:
+        st.write('Coffee bean: ', coffee_bean_val)
         st.write('Coffee roast: ', coffee_roast_val)
+        st.write('Brewing: ', brewing_val)
+        st.write('Milk: ', milk_val)
 
 st.write('Outside the form')
+st.write(brewing_val)
 
 #st.header('Declared using object notation')
 #form = st.form('my_form_2')
