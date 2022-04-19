521b28684d649929727b97fe5e5ed895e8b529f7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-19 14:56:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ with st.form('my_form'):
     st.write('**Order your coffee**')
     
     coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-    cofee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
     brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
     milk_val = st.checkbox('Milk')
 
